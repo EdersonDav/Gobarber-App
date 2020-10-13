60530e06a9315388e3db0f1ad65638a01bd9cfb4
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-10-13 12:33:34 -0300

    First commit

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { Container, Title } from './style';
+
+import logo from '../../assets/logo.png';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logo} />
+      <Title>Faça seu logon</Title>
+    </Container>
+  );
+};
+
+export default SignIn;
