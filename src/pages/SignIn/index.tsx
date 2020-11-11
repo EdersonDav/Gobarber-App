d9f109678846a96da7cdde1a547a1e3271702af9
EdersonDav <silva.edersonqueiroz@gmail.com>
2020-11-11 10:38:34 -0300

    Update

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container, Title } from './style';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logo from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './style';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logo} />
-      <Title>Faça seu logon</Title>
-    </Container>
+    <>
+      <Container>
+        <Image source={logo} />
+        <Title>Faça seu logon</Title>
+        <Input name="mail" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+        <Button
+          onPress={() => {
+            console.log('foi');
+          }}
+        >
+          Entrar
+        </Button>
+        <ForgotPassword
+          onPress={() => {
+            console.log('hello');
+          }}
+        >
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+      <CreateAccountButton
+        onPress={() => {
+          console.log('hello');
+        }}
+      >
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
